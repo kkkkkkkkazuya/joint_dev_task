11e5f3564a355877ed066dc35952e02b3190851b
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2020-05-04 21:53:02 +0900

    Q1, Q2

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斎藤")
+  names << "斎藤"
   puts names[4]
 end
 
@@ -14,9 +14,8 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array1.push(array2)
-  array1.flatten!
-  puts array1
+  puts array1 + array2
+
 end
 
 def q3
@@ -48,7 +47,7 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  ?
+
 end
 
 def q7
@@ -57,7 +56,7 @@ def q7
   # 以下に回答を記載
 
   # 以下は変更しないで下さい
-  p array.to_i
+  p array
 end
 
 def q8
